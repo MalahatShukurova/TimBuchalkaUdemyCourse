69364753fc2f5d57cfe2b4b3e19b80bdb17ebb96
Malahat Shukurova <malakhat.shukurova@mail.ru>
2022-07-20 17:27:49 +0400

    prime number

--- src/academy/learnprogramming/section5/forStatement.java
+++ src/academy/learnprogramming/section5/forStatement.java
@@ -16,7 +16,18 @@ package academy.learnprogramming.section5;
 //            }
             for (int i=8;i>1;i--){
                 System.out.println("10,000 at "+ i + "% interest = "+ String.format("%.2f",calculateInterest(10000,i)));
-            }
+            }}
+
+            public static boolean isPrime(int n){
+                if(n==1){
+                    return false;
+                }
+                for (int i =2 ; i<=n/2; i++)
+                {
+                    if (n%i==0){
+                        return false;
+                    }
+                } return true;
         }
         public static double calculateInterest(double amount, double interestRate){
             return (amount * (interestRate/100));
