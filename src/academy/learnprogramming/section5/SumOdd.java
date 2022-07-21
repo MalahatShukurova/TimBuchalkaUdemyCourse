5d1d114f025ea69078c9d79bac925624048ee18c
Malahat Shukurova <malakhat.shukurova@mail.ru>
2022-07-21 17:04:36 +0400

    sum odd

---
+++ src/academy/learnprogramming/section5/SumOdd.java
@@ -0,0 +1,33 @@
+package academy.learnprogramming.section5;
+
+public class SumOdd {
+    public static void main(String[] args) {
+        System.out.println((sumOdd(1, 100)));
+    }
+    public static boolean isOdd(int number){
+        if(number<0){
+            return false;
+        }
+        return number%2==1;
+    }
+
+    public static int sumOdd(int start, int end){
+        if (end<start||start<0){
+            return -1;
+        }
+        int sum = 0;
+        for (int i = start; i<=end; i++){
+            if (isOdd(i))
+                sum+=i;
+        }
+        return sum;
+
+    }
+
+
+
+
+
+
+
+}
