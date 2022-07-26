e73bdbd18686cc637af2047a3752bc55d2184f37
Malahat Shukurova <malakhat.shukurova@mail.ru>
2022-07-26 17:28:40 +0400

    whileAndDoRecap

--- src/academy/learnprogramming/section5/TheWhileAndDoWhileStatements.java
+++ src/academy/learnprogramming/section5/TheWhileAndDoWhileStatements.java
@@ -1,5 +1,72 @@
 package academy.learnprogramming.section5;
 
 public class TheWhileAndDoWhileStatements {
+    public static void main(String[] args) {
+//        int count = 6;
+//        while(count!=6){
+//            System.out.println("Count value is "+ count);
+//            count++;
+//        }
+//        for (int i= 6; i!=6; i++){
+//            System.out.println("count");
+//        }
+
+//        for (count=1;count!=6;count++){
+//            System.out.println("Count value is "+count);
+//        }
+
+//        count =1;
+//        while (true){
+//            if (count==6){
+//                break;
+//            }
+//            System.out.println("Count value is "+ count );
+//            count++;
+//        }
+//        count =6;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if (count>100){
+//                break;
+//            }
+//        }while (count!=6);
+
+
+//        int number =4;
+//        int finishNumber = 20;
+//
+//        while (number<=finishNumber){
+//            number++;
+//            if (!isEvenNumber(number)){
+//                continue;
+//            }
+//            System.out.println("Even number "+ number);
+//        }
+        int number =4;
+        int finishNumber = 20;
+        int evenNumbersFound=0;
+
+        while (number<=finishNumber){
+            number++;
+            if (!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number "+ number);
+            evenNumbersFound++;
+            if (evenNumbersFound>=5){
+                break;
+            }
+
+        }
+        System.out.println("Total even numbers found "+ evenNumbersFound);
+
+
+    }
+    public static boolean isEvenNumber(int number){
+        return number % 2 == 0;
+    }
+
 
 }
